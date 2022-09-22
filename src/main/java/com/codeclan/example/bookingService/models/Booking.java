75865745878453bb767f8fd391229c23f5794424
“lawriecarly” <Lawrie.davidson91@gmail.com>
2022-09-22 10:53:06 +0100

    added Booking class

--- src/main/java/com/codeclan/example/bookingService/models/Booking.java
+++ src/main/java/com/codeclan/example/bookingService/models/Booking.java
@@ -1,12 +1,64 @@
 package com.codeclan.example.bookingService.models;
+import java.util.Date;
 
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Bookings")
 public class Booking {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    Course course;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    Customer customer;
+
+    public Booking(){}
+
+    public Booking(Date date, Course course, Customer customer) {
+        this.date = date;
+        this.course = course;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
